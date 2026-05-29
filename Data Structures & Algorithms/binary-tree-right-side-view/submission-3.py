77010896d47e6f46e3ebf250e90aff409315bc31
Shashank Bhat <74356928+shashanksde@users.noreply.github.com>
2026-05-28 20:54:31 -0700

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if not root:
+            return res
+
+        q = deque()
+        q.append(root)
+
+        while q:
+            rightside = None #can optimize here, I just need the last value in that level.
+            for _ in range(len(q)):
+                node = q.popleft()
+                
+                if node:
+                    rightside = node
+                    q.append(node.left)
+                    q.append(node.right)
+            #after processing this level if the rightside is non null
+            if rightside:
+                res.append(rightside.val)
+        return res
