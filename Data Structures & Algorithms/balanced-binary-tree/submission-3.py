62bca56b6f90bc16ebb7cdd254ba41c41bc407f7
Shashank Bhat <74356928+shashanksde@users.noreply.github.com>
2026-06-27 23:02:26 -0700

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check(node):
+            if not node:
+                return 0
+            
+            left = check(node.left)
+            right = check(node.right)
+            
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            
+            return max(left, right) + 1 #height of that node
+            
+        return check(root) != -1
